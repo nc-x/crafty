8405c9ad8097af2753d8f6f466a5325be8df2500
Neelesh Chandola <neelesh.chandola@outlook.com>
2018-06-13 08:40:31 +0530

    Chapter 5. Representing Code

---
+++ src/craftypkg/astprinter.nim
@@ -0,0 +1,45 @@
+{.this: self.}
+
+import expr, literalType
+
+type
+  AstPrinter = ref object of RootObj
+
+proc parenthesize(self: AstPrinter, name: string, exprs: varargs[Expr]): string
+
+method print*(self: AstPrinter, expr: Expr): string {.base.}=
+  discard
+
+method print*(self: AstPrinter, expr: Binary): string =
+  return parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+method print*(self: AstPrinter, expr: Grouping): string =
+  return parenthesize("group", expr.expression)
+
+method print*(self: AstPrinter, expr: Literal): string =
+  return $expr.value
+
+method print*(self: AstPrinter, expr: Unary): string =
+  return parenthesize(expr.operator.lexeme, expr.right)
+
+proc parenthesize(self: AstPrinter, name: string, exprs: varargs[Expr]): string =
+  result = "(" & name
+
+  for expr in exprs:
+    result &= " "
+    result &= print(expr)
+  
+  result &= ")"
+
+when isMainModule:
+  import token, tokentype, literalType
+  let expression = newBinary(
+    newUnary(
+      newToken(MINUS, "-", newNilLit(), 1),
+      newLiteral(newNumLit(123))
+    ),
+    newToken(STAR, "*", newNilLit(), 1),
+    newGrouping(newLiteral(newNumLit(45.67)))
+  )
+  let printer = AstPrinter()
+  echo printer.print(expression)
---
+++ src/craftypkg/expr.nim
@@ -0,0 +1,32 @@
+import token
+import literalType
+
+type
+  Expr* = ref object of RootObj
+    
+  Binary* = ref object of Expr
+    left*: Expr
+    operator*: Token
+    right*: Expr
+
+  Grouping* = ref object of Expr
+    expression*: Expr
+  
+  Literal* = ref object of Expr
+    value*: LiteralType
+  
+  Unary* = ref object of Expr
+    operator*: Token
+    right*: Expr
+
+proc newBinary*(left: Expr, operator: Token, right: Expr): Binary=
+  result = Binary(left: left, operator: operator, right: right)
+
+proc newGrouping*(expression: Expr): Grouping=
+  result = Grouping(expression: expression)
+
+proc newLiteral*(value: LiteralType): Literal=
+  result = Literal(value: value)
+
+proc newUnary*(operator: Token, right: Expr): Unary=
+  result = Unary(operator: operator, right: right)
