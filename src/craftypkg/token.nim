ac504f049a0e29350e4740283faa11983975c27f
Neelesh Chandola <neelesh.chandola@outlook.com>
2019-04-12 13:02:21 +0530

    Update for latest nim devel

--- src/craftypkg/token.nim
+++ src/craftypkg/token.nim
@@ -13,4 +13,4 @@ proc newToken*(tkType: TokenType, lexeme: string, literal: LiteralType, line: in
   return Token(tkType: tkType, lexeme: lexeme, literal: literal, line: line)
 
 proc `$`*(self: Token): string =
-  return &"Line: {self.line}, TokenType: {self.tkType}, Lexeme: {self.lexeme}, Literal: {self.literal}"
+  return &"Line: {self.line}, TokenType: {self.tkType}, Lexeme: {self.lexeme}, Literal: {$self.literal}"
